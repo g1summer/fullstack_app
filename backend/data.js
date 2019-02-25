2799dd32af82b51b6627f26c3534c4e54f491659
g1summer <haitaoran@hotmail.com>
2019-02-25 09:17:40 -0500

    initial commit

---
+++ backend/data.js
@@ -0,0 +1,36 @@
+
+// /backend/data.js
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+
+
+// this will be our data base's data structure 
+
+const DataSchema = new Schema(
+
+  {
+
+    id: Number,
+	x: Number,
+	y: Number,
+	z: Number,
+	r: Number,
+	g: Number,
+	b: Number,
+
+    message: String
+
+  },
+
+  { timestamps: true }
+
+);
+
+
+
+// export the new Schema so we could modify it using Node.js
+
+module.exports = mongoose.model("Data", DataSchema);
